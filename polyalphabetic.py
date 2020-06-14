9d0c17b3a92760716bf9b0e2fb9ffebca9d58d99
skelegorg <cumminan@northvilleschools.net>
2020-06-14 10:26:14 -0700

    Added full polyalphabetic encryption capability

--- polyalphabetic.py
+++ polyalphabetic.py
@@ -6,30 +6,45 @@ def regenerateKey(length=10):
     # Generate a couple random numbers and save them on different lines
     keyFile = open("key.txt", "w")
     loopHelper = 0
-    while(loopHelper < 5):
-        keyFile.write(str(secrets.randbelow(length)) + '\n')
+    while(loopHelper < length):
+        keyFile.write(str(secrets.randbelow(6)) + '\n')
         loopHelper += 1
 
 
 def encrypt(fileToEncrypt):
     # Open files
-    keyFile = open("key.txt", "r")
+    try:
+        keyFileTest = open("key.txt", "r")
+        # fix an issue with line 24
+        fileExists = True
+        keyFileTest.close()
+    except FileNotFoundError:
+        # if no key file exists, create a key file and start the function over
+        regenerateKey()
+        encrypt(fileToEncrypt)
+    # if the keyfile exists, open it up
+    if(fileExists == True):
+        keyFile = open("key.txt", "r")
+    # open the file to encrypt
     encryptFile = open(fileToEncrypt, "r")
+    # if the keyfile is not empty
     if(keyFile.read() != ''):
         keyFile.close()
         # Nomial path:
-        # Saves the unencrypted password file
+        # Saves the unencrypted file
         encryptArray = []
         for line in encryptFile:
+            # save on a line-to-an-index basis
             currentLine = line.strip()
             encryptArray.append(currentLine)
         encryptFile.close()
+        # now that the file is saved, empty it out
         encryptFile = open(fileToEncrypt, "w")
         encryptFile.close()
         elemLoopHelper = 0
         keyArray = []
         keySwitcher = 0
-        # Populate the keyArray with all 5 digits of the key
+        # Populate the keyArray with all the digits of the key
         with open("key.txt", "r") as keyFile:
             for line in keyFile:
                 keyLine = line.strip()
@@ -48,70 +63,77 @@ def encrypt(fileToEncrypt):
                 enCharS = chr(enChar)
                 # Add each newly encrypted character to the encryptedLine string
                 encryptedLine += enCharS
-                # Change the line being read by the polyNumAdd
+                # Change the character being read by the polyNumAdd
                 keySwitcher += 1
-                if (keySwitcher == 5):
+                if (keySwitcher == 10):
                     keySwitcher = 0
+            # replace the unencrypted line of the array with the encrypted line
             encryptArray[elemLoopHelper] = encryptedLine
             # Change the slot of the array that is being encrypted
             elemLoopHelper += 1
         with open(fileToEncrypt, "a") as pwf:
             for element in encryptArray:
                 pwf.writelines(element + '\n')
-        print("Encrypted data")
     else:
         # No key, so generate one
         regenerateKey()
+        # start over
         encrypt(fileToEncrypt)
 
 
 def decrypt(fileToDecrypt):
     # Open files
-    keyFile = open("key.txt", "r")
-    encryptFile = open(fileToDecrypt, "r")
+    try:
+        keyFile = open("key.txt", "r")
+    except FileNotFoundError:
+        raise FileNotFoundError("no key file was found in the directory")
+    # open the file to decrypt
+    decryptFile = open(fileToDecrypt, "r")
+    # if the key file was not empty, start the decrypting process
     if(keyFile.read() != ''):
         keyFile.close()
         # Nomial path:
         # Saves the unencrypted password file
-        encryptArray = []
-        for line in encryptFile:
+        decryptArray = []
+        # populate the decryptArray with on a line-to-an-index basis
+        for line in decryptFile:
             currentLine = line.strip()
-            encryptArray.append(currentLine)
-        encryptFile.close()
-        encryptFile = open(fileToDecrypt, "w")
-        encryptFile.close()
+            decryptArray.append(currentLine)
+        decryptFile.close()
+        # clear the file
+        decryptFile = open(fileToDecrypt, "w")
+        decryptFile.close()
         elemLoopHelper = 0
         keyArray = []
         keySwitcher = 0
-        # Populate the keyArray with all 5 digits of the key
+        # Populate the keyArray with all the digits of the key
         with open("key.txt", "r") as keyFile:
             for line in keyFile:
                 keyLine = line.strip()
                 keyArray.append(keyLine)
         # for each line in the password file
-        for element in encryptArray:
+        for element in decryptArray:
             # Reset/create a string which holds the current encrypted line
             decryptedLine = ''
             # For each character in each line of the password file
-            for char in encryptArray[elemLoopHelper]:
+            for char in decryptArray[elemLoopHelper]:
                 # Figure out which element of the key is being added
                 polyAdd = int(keyArray[keySwitcher])
                 # Go through key values and add them to each
                 deChar = ord(char) - polyAdd
-                # Convert enChar to enCharS
+                # Convert deChar to deCharS
                 deCharS = chr(deChar)
-                # Add each newly encrypted character to the encryptedLine string
+                # Add each newly decrypted character to the decryptedLine string
                 decryptedLine += deCharS
-                # Change the line being read by the polyNumAdd
+                # Change the character being read by the polyNumAdd
                 keySwitcher += 1
-                if (keySwitcher == 5):
+                if (keySwitcher == 10):
                     keySwitcher = 0
-            encryptArray[elemLoopHelper] = decryptedLine
+            decryptArray[elemLoopHelper] = decryptedLine
             # Change the slot of the array that is being encrypted
             elemLoopHelper += 1
         with open(fileToDecrypt, "a") as pwf:
-            for element in encryptArray:
+            for element in decryptArray:
                 pwf.writelines(element + '\n')
-        print("Decrypted data")
     else:
-        print("Key not detected")
+        raise FileNotFoundError("no key file was found in the directory")
