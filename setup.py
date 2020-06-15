860cd0e84898d70b109a4a4291e96ccc8fcb64a7
skelegorg <cumminan@northvilleschools.net>
2020-06-15 09:45:16 -0700

    e

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ classifiers = [
 
 setup(
     name='polyencryption',
-    version='0.0.2',
+    version='0.0.8',
     description='a simple encryption algorithm that encrypts .txt files using a polyalphabetic cypher. NOTE- this software is not intended for or should be used for security purposes',
     long_description=long_description,
     url='https://github.com/skelegorg/polyencryption',
@@ -28,5 +28,5 @@ setup(
     classifiers=classifiers,
     keywords='encryption',
     packages=find_packages(),
-    install_requires=['']
+    install_requires=['secrets>=3']
 )
